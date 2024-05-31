ec382f62f32618a7afe1501e61dac6cda0c0bd62
Sean Gulliver Doctora <freedata04@gmail.com>
2024-05-31 21:45:12 +0800

    commit

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,10 +2,10 @@
     <li class="nav-item">
         <a href="{{ route('home') }}" class="nav-link d-flex align-items-center">
             <span class="sidebar-icon me-3">
-                <img src="{{ asset('images/brand/light.svg') }}" height="20" width="20" alt="Volt Logo">
+                <img src="{{ asset('images/logo/kayanlog.png') }}" height="80" width="80" alt="Volt Logo">
             </span>
             <span class="mt-1 ms-1 sidebar-text">
-                Volt Laravel
+                Barangay <br> Management <br> Information <br> System
             </span>
         </a>
     </li>
